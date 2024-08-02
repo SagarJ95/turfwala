0029b55babf5f04f43f1577f76328e11a881b9af
SagarJ95 <155131844+SagarJ95@users.noreply.github.com>
2024-08-02 13:20:43 +0530

    image compression

--- Controller/AdminApiController.js
+++ Controller/AdminApiController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require("../db/database");
 const { validationResult } = require("express-validator");
+const sharp = require("sharp");
 
 module.exports.save_turf = async (req, res) => {
   try {
@@ -55,18 +56,24 @@ module.exports.save_turf = async (req, res) => {
 
     if (turfId) {
       //upload Exterior Design
-      const exteriorImage = req.files.exterior;
+      const { buffer, exteriorImage } = req.files.exterior;
 
       if (exteriorImage) {
         for (var i = 0; i < exteriorImage.length; i++) {
-          const exteriorImageName = exteriorImage[i].filename;
+          const exteriorImageName = exteriorImage[i].originalname;
+
           if (exteriorImageName) {
             const exteriorImageData = {
-              media_path: "exterior/" + exteriorImageName,
+              media_file: `${exteriorImage[i].originalname}.webp`,
+              media_path: `${"exterior/"}${exteriorImageName}.webp`,
               media_type: 2, //media type table used 2 for exterior
               truf_id: turfId.row[0].id,
               created_by: Date.now(),
             };
+
+            await sharp(buffer)
+              .webp({ quality: 20 })
+              .toFile("storage/exterior/" + exteriorImageData.media_file);
 
             const ExteriorId = await db.query(
               "Insert into turf_media(media_path,media_type,truf_id) VALUES(media_path,media_type,truf_id) RETURNING *",
@@ -81,77 +88,77 @@ module.exports.save_turf = async (req, res) => {
       }
 
       //Branding Image
-      const brandingImage = req.files.branding;
-      if (brandingImage) {
-        for (const j = 0; j < brandingImage.length; j++) {
-          const brandingImageName = brandingImage[j].filename;
-          if (brandingImageName) {
-            const brandingImageData = {
-              media_path: "branding/" + brandingImageName,
-              media_type: 3,
-              turf_id: turfId.row[0].id,
-            };
+      // const brandingImage = req.files.branding;
+      // if (brandingImage) {
+      //   for (const j = 0; j < brandingImage.length; j++) {
+      //     const brandingImageName = brandingImage[j].filename;
+      //     if (brandingImageName) {
+      //       const brandingImageData = {
+      //         media_path: "branding/" + brandingImageName,
+      //         media_type: 3,
+      //         turf_id: turfId.row[0].id,
+      //       };
 
-            const brandingImageId = await db.query(
-              "Insert into turf_media(media_path,media_type,turf_id) values (?,?,?) RETURNING *",
-              [
-                brandingImageData.media_path,
-                brandingImageData.media_type,
-                brandingImageData.turf_id,
-              ]
-            );
-          }
-        }
-      }
+      //       const brandingImageId = await db.query(
+      //         "Insert into turf_media(media_path,media_type,turf_id) values (?,?,?) RETURNING *",
+      //         [
+      //           brandingImageData.media_path,
+      //           brandingImageData.media_type,
+      //           brandingImageData.turf_id,
+      //         ]
+      //       );
+      //     }
+      //   }
+      // }
 
       // Interior Image
-      const InteriorImage = req.files.interior;
-      if (InteriorImage) {
-        for (const k = 0; k < InteriorImage.length; k++) {
-          const interiorImg = InteriorImage[k].filename;
+      // const InteriorImage = req.files.interior;
+      // if (InteriorImage) {
+      //   for (const k = 0; k < InteriorImage.length; k++) {
+      //     const interiorImg = InteriorImage[k].filename;
 
-          if (interiorImg) {
-            const interiorData = {
-              media_path: "interior/" + interiorImg,
-              media_type: 2,
-              turf_id: turfId.row[0].id,
-            };
-          }
+      //     if (interiorImg) {
+      //       const interiorData = {
+      //         media_path: "interior/" + interiorImg,
+      //         media_type: 2,
+      //         turf_id: turfId.row[0].id,
+      //       };
+      //     }
 
-          const interiorQuery = await db.query(
-            "insert into turf_media(media_path,media_type,turf_id) values(?,?,?)",
-            [
-              interiorData.media_path,
-              interiorData.media_type,
-              interiorData.turf_id,
-            ]
-          );
-        }
-      }
+      //     const interiorQuery = await db.query(
+      //       "insert into turf_media(media_path,media_type,turf_id) values(?,?,?)",
+      //       [
+      //         interiorData.media_path,
+      //         interiorData.media_type,
+      //         interiorData.turf_id,
+      //       ]
+      //     );
+      //   }
+      // }
 
       //Pamphlet Image
-      const PamphletImage = req.files.pamphlet;
-      if (PamphletImage) {
-        for (const p = 0; p < PamphletImage.length; p++) {
-          const pamphletImg = PamphletImage[p].filename;
-          if (pamphletImg) {
-            const pamphletData = {
-              media_path: "pamphlet/" + pamphletImg,
-              media_type: 4,
-              turf_id: turfId.row[0].id,
-            };
-          }
+      // const PamphletImage = req.files.pamphlet;
+      // if (PamphletImage) {
+      //   for (const p = 0; p < PamphletImage.length; p++) {
+      //     const pamphletImg = PamphletImage[p].filename;
+      //     if (pamphletImg) {
+      //       const pamphletData = {
+      //         media_path: "pamphlet/" + pamphletImg,
+      //         media_type: 4,
+      //         turf_id: turfId.row[0].id,
+      //       };
+      //     }
 
-          const pamphletId = await db.query(
-            "Insert Into turf_media(media_path,media_type,turf_id) Values(?,?,?)",
-            [
-              pamphletData.media_path,
-              pamphletData.media_type,
-              pamphletData.turf_id,
-            ]
-          );
-        }
-      }
+      //     const pamphletId = await db.query(
+      //       "Insert Into turf_media(media_path,media_type,turf_id) Values(?,?,?)",
+      //       [
+      //         pamphletData.media_path,
+      //         pamphletData.media_type,
+      //         pamphletData.turf_id,
+      //       ]
+      //     );
+      //   }
+      // }
 
       res.status(200).json({ message: "Turf saved successfully" });
     }
