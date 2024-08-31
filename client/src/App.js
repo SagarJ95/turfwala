87345f3cf03e2b57571275520338614c68f6e7c5
SagarJ95 <155131844+SagarJ95@users.noreply.github.com>
2024-08-31 12:41:26 +0530

    Changes in home and truf_deatisl route and page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
-import React, { Fragment } from 'react';
-import Home from './components/layout/Home';
-import Banner from './components/layout/banner';
-import Turf from './components/layout/turf';
-import Top_Turf from './components/layout/top_turf';
-import List_Turf from './components/layout/list_turf';
-import './App.css';
+import React, { Fragment } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  swith,
+} from "react-router-dom";
+import Home from "./components/layout/Home";
+import TurfDetails from "./components/layout/turf_details";
+// import Banner from './components/layout/banner';
+// import Turf from './components/layout/turf';
+// import Top_Turf from './components/layout/top_turf';
+// import List_Turf from './components/layout/list_turf';
+import "./App.css";
 
-const App = () =>
-  <Fragment>
-    <Home />
-    <Banner />
-    <Turf />
-    <Top_Turf />
-    <List_Turf />
-  </Fragment>
+const App = () => (
+  <Router>
+    <Fragment>
+      <Routes>
+        <Route exact path="/" Component={Home} />
+        <Route exact path="/turf_details" Component={TurfDetails} />
+      </Routes>
+    </Fragment>
+  </Router>
+);
 
 export default App;
---
+++ client/src/components/layout/turf_details.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const trufDetails = () => {
+  return (
+    <>
+      <section className="why-us-sec">
+        <div className="container">
+          <h3>Why Choose Us?</h3>
+          <div className="row">
+            <div className="col-lg-3">
+              <img src="assets/images/home/device.svg" alt="image" />
+              <h5>Special Financing Offers</h5>
+              <p>
+                Our stress-free finance department that can find financial
+                solutions to save you money.
+              </p>
+            </div>
+            <div className="col-lg-3">
+              <img src="assets/images/home/device.svg" alt="image" />
+              <h5>Special Financing Offers</h5>
+              <p>
+                Our stress-free finance department that can find financial
+                solutions to save you money.
+              </p>
+            </div>
+            <div className="col-lg-3">
+              <img src="assets/images/home/device.svg" alt="image" />
+              <h5>Special Financing Offers</h5>
+              <p>
+                Our stress-free finance department that can find financial
+                solutions to save you money.
+              </p>
+            </div>
+            <div className="col-lg-3">
+              <img src="assets/images/home/device.svg" alt="image" />
+              <h5>Special Financing Offers</h5>
+              <p>
+                Our stress-free finance department that can find financial
+                solutions to save you money.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default trufDetails;
